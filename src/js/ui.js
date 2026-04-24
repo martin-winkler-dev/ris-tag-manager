74bf4876a4a988071ac587ecd14a0d8c0c3286cb
smartin <209699316+martin-winkler@users.noreply.github.com>
2026-04-24 22:13:58 +0200

    feat(ui): implement minimal ui builder

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,1 +1,66 @@
-// ui
+// minimal ui builder
+function buildUI(definition) {
+	const refs = {};
+
+	if (Array.isArray(definition)) {
+		const fragment = document.createDocumentFragment();
+
+		for (const childDefinition of definition) {
+			fragment.appendChild(createNode(childDefinition, refs));
+		}
+
+		return { root: fragment, refs };
+	}
+
+	return {
+		root: createNode(definition, refs),
+		refs,
+	};
+
+    function createNode(definition, refs) {
+        if (!definition || typeof definition !== "object") {
+            throw new Error("Definition not an object.");
+        }
+        
+        const {
+            tag = "div",
+            content = "",
+            attrs = {},
+            children = [],
+            ref,
+            on = {},
+        } = definition;
+        
+        const element = document.createElement(tag);
+        
+        if (content !== "") {
+            element.textContent = String(content);
+        }
+        
+        // attributes
+        for (const [name, value] of Object.entries(attrs)) {
+            if (value === false || value === null || value === undefined) {
+                continue;
+            }
+    
+            if (value === true) {
+                element.setAttribute(name, "");
+                continue;
+            }
+    
+            element.setAttribute(name, String(value));
+        }
+
+        // children
+        for (const childDefinition of children) {
+            element.appendChild(createNode(childDefinition, refs));
+        }
+
+        // add ref to html element
+        if (ref && typeof ref === "string") {
+            refs[ref] = element;
+        }
+        
+        return element;
+    }
+}

fddb480c21d1f93c1e1cc9e37877fc7fb0c366fa
smartin <209699316+martin-winkler@users.noreply.github.com>
2026-04-25 01:35:14 +0200

    feat(ris): implement outline

--- src/js/ris.js
+++ src/js/ris.js
@@ -1,1 +1,25 @@
 // RIS parsing and tag-delete
+
+export function parseFile(fileContent) {
+    if (!fileContent) {
+        throw new Error("Missing file content.");
+    }
+
+    const tags = { kw: {count: 3 } };
+
+    return {
+        paperCount: 0,
+        tags: tags,
+    }
+}
+
+export function deleteTag(fileContent, tag) {
+    if (!fileContent) {
+        throw new Error("Missing file content.");
+    }
+    if (!tag) {
+        throw new Error("Missing tag.");
+    }
+
+    return fileContent;
+}

213e8dd6cccb1c9c68f0bf3fb62284c4945a10d0
smartin <209699316+martin-winkler@users.noreply.github.com>
2026-04-25 16:40:07 +0200

    feat(tags): add more default tags

--- src/js/tags.js
+++ src/js/tags.js
@@ -1,8 +1,76 @@
 // default RIS tags
 export const TAG_CONFIG = Object.freeze({
+    TY: Object.freeze({
+        name: "Type of Reference", // first tag -- dont display
+    }),
+    AU: Object.freeze({
+        name: "Author",
+    }),
+    TI: Object.freeze({
+        name: "Title",
+    }),
+    T2: Object.freeze({
+        name: "Secondary Title (Journal/Book)",
+    }),
+    PY: Object.freeze({
+        name: "Publication Year",
+    }),
+    VL: Object.freeze({
+        name: "Volume",
+    }),
+    IS: Object.freeze({
+        name: "Issue",
+    }),
+    SP: Object.freeze({
+        name: "Start Page",
+    }),
+    EP: Object.freeze({
+        name: "End Page",
+    }),
+    SN: Object.freeze({
+        name: "ISSN/ISBN",
+    }),
+    DO: Object.freeze({
+        name: "DOI",
+    }),
+    UR: Object.freeze({
+        name: "URL",
+    }),
+    AB: Object.freeze({
+        name: "Abstract",
+    }),
     KW: Object.freeze({
         name: "Keywords",
-        display: "marked",
+    }),
+    N1: Object.freeze({
+        name: "Notes",
+    }),
+    N2: Object.freeze({
+        name: "Abstract/Notes (Secondary)",
+    }),
+    AN: Object.freeze({
+        name: "Accession Number",
+    }),
+    DB: Object.freeze({
+        name: "Database Name",
+    }),
+    DP: Object.freeze({
+        name: "Database Provider",
+    }),
+    LA: Object.freeze({
+        name: "Language",
+    }),
+    CY: Object.freeze({
+        name: "City",
+    }),
+    PB: Object.freeze({
+        name: "Publisher",
+    }),
+    AD: Object.freeze({
+        name: "Author Address",
+    }),
+    ER: Object.freeze({
+        name: "End of Record", // last tag -- dont display
     }),
 });
 
